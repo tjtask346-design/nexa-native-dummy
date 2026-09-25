081e4c9262298a4996f8579752b9c98ea02ba640
Mr AiM <tjtask346@gmail.com>
2026-09-25 08:56:16 +0600

    Downgrade Kotlin compiler extension version for Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         jvmTarget = "17"
     }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
